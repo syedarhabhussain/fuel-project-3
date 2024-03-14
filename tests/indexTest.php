c1066e516100afb4075a3f98d4b96f59c4a09433
syedarhabhussain <syedarhabhussain>
2024-03-15 04:11:56 +0500

    Project 3

--- tests/indexTest.php
+++ tests/indexTest.php
@@ -6,7 +6,7 @@ class LoginFormTest extends TestCase
     public function testHeaderInclude()
     {
         ob_start();
-        include __DIR__ . '/header.php';
+        include __DIR__ . '/../src/header.php';
         $output = ob_get_clean();
         $this->assertStringContainsString('<header', $output);
     }
